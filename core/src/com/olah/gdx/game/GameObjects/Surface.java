eeb6a5bca89716378998159ba58106d0076457bd
Salvesti <olah.brad@gmail.com>
2016-10-31 08:11:34 -0400

    Replaced a file that was not committed and somehow lost.

---
+++ core/src/com/olah/gdx/game/GameObjects/Surface.java
@@ -0,0 +1,18 @@
+package com.olah.gdx.game.GameObjects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Surface extends AbstractGameObject
+{
+
+	protected void init()
+	{
+		this.type = "surface";
+	}
+	@Override
+	public void render(SpriteBatch batch)
+	{
+		// TODO Auto-generated method stub
+	}
+
+}
